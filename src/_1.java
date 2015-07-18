e5cfa5e755bdbc80f92ec331d9cb558b0179cfe6
Ahmed Mahmoud <ahmed.yousef991@gmail.com>
2015-07-18 14:13:23 +0200

    Spoj

---
+++ src/_1.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+//http://www.spoj.com/problems/TEST/
+//TEST - Life, the Universe, and Everything
+
+public class _1 {
+
+	public static void main(String[] args) {
+
+		try {
+			BufferedReader bufferRead = new BufferedReader(new InputStreamReader(System.in));
+			String input;
+
+			input = bufferRead.readLine();
+			while (!input.equals("42")) {
+				System.out.println(input);
+				input = bufferRead.readLine();
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+}
---
+++ src/_2.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+//http://www.spoj.com/problems/ADDREV/
+//ADDREV - Adding Reversed Numbers
+
+public class _2 {
+
+	public static void main(String[] args) {
+
+		try {
+			BufferedReader bufferRead = new BufferedReader(new InputStreamReader(System.in));
+
+			int counter = Integer.parseInt(bufferRead.readLine());
+			String[] values;
+			while (counter > 0) {
+				values = (bufferRead.readLine()).split(" ");
+				int firstValues = Integer.parseInt(new StringBuilder(values[0]).reverse().toString());
+				int secondValues = Integer.parseInt(new StringBuilder(values[1]).reverse().toString());
+				int sum = firstValues + secondValues;
+				System.out.println(Integer.parseInt(new StringBuilder(sum + "").reverse().toString()+""));
+				counter--;
+
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+}
